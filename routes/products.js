b0d04fbfdd0d8c20aa1125314ca765b016ae15fb
Jan Nowak <jannowak@example.com>
2019-11-29 13:40:15 +0100

    product

---
+++ routes/products.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const router = express.Router();
+const authMid = require('../middleware/authorization');
+const productService = require("../service/productService");
+
+// add
+router.post('/', authMid, (req, res, next) => {
+    const productData = req.body;
+
+    productService.add(productData)
+        .then(product => res.status(201).json(product))
+        .catch(err => next(err));
+});
+
+// list
+router.get('/', authMid, (req, res, next) => {
+    productService.getAll()
+        .then(product => res.json(product))
+        .catch(err => next(err));
+});
+
+
+module.exports = router;
---
+++ service/productService.js
@@ -0,0 +1,25 @@
+const Product = require("../model/product");
+const resolveErrorType = require('../error').resolveErrorType;
+const logger = require('../libs/logger');
+
+const productService = {
+    getAll: () => {
+        return new Promise((resolve, reject) => {
+            Product.find()
+                .populate('data')
+                .then(resolve)
+                .catch(err => reject(err))
+        });
+    },
+    
+    add: (productData) => {
+        return new Promise((resolve, reject) => {
+            logger.debug(`Adding carnet item: `, productData);
+            new Product(productData).save()
+                .then(resolve)
+                .catch(err => reject(resolveErrorType(err)))
+        });
+    },
+};
+
+module.exports = carnetService;
