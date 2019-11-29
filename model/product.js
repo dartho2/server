fa706203aee858836ff5b8f516ac5e7dae971bdb
Jan Nowak <jannowak@example.com>
2019-11-29 15:01:10 +0100

    product ser

--- model/product.js
+++ model/product.js
@@ -2,7 +2,26 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const productSchema = new Schema({
+    
+        name: {type: String, required: true, unique: true},
+        description: {type: String, default: ""},
+        label: {type: String, required: true},
+        logo: {type: String, default: null},
+        image: {type: Array, default: {}},
+        style: {type: Object, default: {}},
+        active: {type: Boolean, default: true},
+        price: {type: Number, required: true },
+        unit: {type: String, required: true}
    
 });
+// Getter
+productSchema.path('price').get(function(num) {
+    return (num / 100).toFixed(2);
+  });
+  
+  // Setter
+  productSchema.path('price').set(function(num) {
+    return num * 100;
+  });
 
 module.exports = mongoose.model('Products', productSchema);
