bf53c866c49ff40a30b39d376f4693789b8b4de3
Jan Nowak <jannowak@example.com>
2019-11-29 15:01:10 +0100

    product ser

--- service/productService.js
+++ service/productService.js
@@ -6,20 +6,45 @@ const productService = {
     getAll: () => {
         return new Promise((resolve, reject) => {
             Product.find()
-                .populate('data')
                 .then(resolve)
                 .catch(err => reject(err))
         });
     },
-    
-    add: (productData) => {
+    add: (productItemData) => {
         return new Promise((resolve, reject) => {
-            logger.debug(`Adding carnet item: `, productData);
-            new Product(productData).save()
+            logger.debug(`Adding content item: `, productItemData);
+            new product(productItemData).save()
                 .then(resolve)
                 .catch(err => reject(resolveErrorType(err)))
         });
     },
-};
+        
+        remove: (productItemId) => {
+            return new Promise((resolve, reject) => {
+                Product.findOne({_id: productItemId})
+                    .then(productItem => {
+                        if (productItem) {
+                            logger.debug(`Removing content item \n ${JSON.stringify(productItem, null, 2)}`);
+                            return productItem.remove();
+                        }
+                    })
+                    .then(resolve)
+                    .catch(err => reject(err));
+            });
+        },
+    
+        update: (id, productItemData) => {
+            const {type, styles, content, tags} = productItemData;
+            console.log(productItemData);
+    
+            return new Promise((resolve, reject) => {
+                logger.debug(`Updating content item ${id} with:\n ${JSON.stringify(productItemData, null, 2)} `);
+    
+                ProductItem.findByIdAndUpdate(id, {type, styles, content, tags}, {runValidators: true})
+                    .then(resolve)
+                    .catch(err => reject(resolveErrorType(err)))
+            });
+        },
+    };
 
 module.exports = productService;
