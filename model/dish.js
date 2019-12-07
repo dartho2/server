9a931a4c2edde642a4af17e4e6b38c6bc7a0514b
Jan Nowak <jannowak@example.com>
2019-12-07 21:04:00 +0100

    dish

---
+++ model/dish.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const dishSchema = new Schema({
+    
+        name: {type: String, required: true, unique: true},
+        price: {type: String,  required: true },
+        products: [{
+            type: Object,  
+            name: {type: String, required: true},
+            unit: {type: String, required: true},
+            price: {type: String, required: true},
+            productweight: {type: String, required: true},
+            valueproduct: {type: String, required: true},
+            weight: {type: String, required: true} 
+        }],
+        
+        
+        
+   
+});
+// // Getter
+// productSchema.path('price').get(function(num) {
+//     return (num / 100).toFixed(2);
+//   });
+  
+//   // Setter
+//   productSchema.path('price').set(function(num) {
+//     return num * 100;
+//   });
+
+module.exports = mongoose.model('Dishes', dishSchema);
---
+++ service/dishService.js
@@ -0,0 +1,56 @@
+const Dish = require("../model/product");
+const resolveErrorType = require('../error').resolveErrorType;
+const logger = require('../libs/logger');
+
+const dishService = {
+    getAll: () => {
+        return new Promise((resolve, reject) => {
+            Dish.find()
+                .then(resolve)
+                .catch(err => reject(err))
+        });
+    },
+    add: (dishItemData) => {
+        return new Promise((resolve, reject) => {
+            new Dish(dishItemData).save()
+                .then(resolve)
+                .catch(err => reject(resolveErrorType(err)))
+        });
+    },
+    get: (id) => {
+        return new Promise((resolve, reject) => {
+            Dish.findById(id)
+                .then(resolve)
+                .catch(err => reject(err))
+        });
+    },
+        
+        remove: (dishItemId) => {
+            return new Promise((resolve, reject) => {
+                Dish.findOne({_id: dishItemId})
+                    .then(dishItem => {
+                        if (dishItem) {
+                            logger.debug(`Removing content item \n ${JSON.stringify(dishItem, null, 2)}`);
+                            return dishItem.remove();
+                        }
+                    })
+                    .then(resolve)
+                    .catch(err => reject(err));
+            });
+        },
+    
+        update: (id, dishItemData) => {
+            const {name,price,products} = dishItemData;
+            console.log(dishItemData);
+    
+            return new Promise((resolve, reject) => {
+                logger.debug(`Updating content item ${id} with:\n ${JSON.stringify(dishItemData, null, 2)} `);
+    
+                Dish.findByIdAndUpdate(id, {name,price,products}, {runValidators: true})
+                    .then(resolve)
+                    .catch(err => reject(resolveErrorType(err)))
+            });
+        },
+    };
+
+module.exports = productService;
