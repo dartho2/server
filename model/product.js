4b661658cc2daab6292b3a9a67cbac69046ab24e
Jan Nowak <jannowak@example.com>
2020-01-08 11:05:08 +0100

    up

--- model/product.js
+++ model/product.js
@@ -16,6 +16,7 @@ const productSchema = new Schema({
         vat: {type: Number, required: true},
         totalPrice: {type: String, required: true},
         product_data: {type: String,required: true},
+        vat: {type: Number,required: true},
         history: {type: Object,
                 required: true},
    
