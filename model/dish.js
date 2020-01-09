9632ea614ed1798643d2d25e6eb022ae14797db7
Jan Nowak <jannowak@example.com>
2020-01-10 00:34:28 +0100

    re

--- model/dish.js
+++ model/dish.js
@@ -4,10 +4,10 @@ const Schema = mongoose.Schema;
 const dishSchema = new Schema({
     
         name: {type: String, required: true, unique: true},
-        price: {type: String,  required: true },
-        price_p: {type: String,  required: true },
-        products: {
-            type: Object,  required: false }
+        foodCost: {type: String,  required: true },
+        bruttoPrice: {type: String,  required: true },
+        productMargin: {type: String,  required: true },
+        products: {type: Object,  required: false }
 });
 
 module.exports = mongoose.model('Dishes', dishSchema);
