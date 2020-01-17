e31918bb2419342120a2589ebdcad8545a2512d8
Jan Nowak <jannowak@example.com>
2020-01-17 18:01:45 +0100

    category

--- model/dish.js
+++ model/dish.js
@@ -5,6 +5,7 @@ const dishSchema = new Schema({
     
         name: {type: String, required: true, unique: true},
         description: {type: String, required: true},
+        category:  {type: String, required: true},
         foodCost: {type: String,  required: true },
         bruttoPrice: {type: String,  required: true },
         productMargin: {type: String,  required: true },
