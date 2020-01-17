71e6fe8a1bbd61c2ca1bf4d6317baed6917bc91a
Jan Nowak <jannowak@example.com>
2020-01-17 14:33:09 +0100

    description

--- model/dish.js
+++ model/dish.js
@@ -4,6 +4,7 @@ const Schema = mongoose.Schema;
 const dishSchema = new Schema({
     
         name: {type: String, required: true, unique: true},
+        description: {type: String, required: true},
         foodCost: {type: String,  required: true },
         bruttoPrice: {type: String,  required: true },
         productMargin: {type: String,  required: true },
