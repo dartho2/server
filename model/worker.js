7bccebe6c994a71d479a83304da5f39d34522288
dartho2 <wegrzyn_pawel@outlook.com>
2020-02-01 17:10:22 +0100

    worker ed

--- model/worker.js
+++ model/worker.js
@@ -2,8 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const workerSchema = new Schema({
-    
-        name: {type: String, required: true, unique: true},
+        users: {type: Array, required: true}
        
 });
 
