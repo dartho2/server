78492358a92af75f5b5d508d59d20df28273a5c9
dartho2 <wegrzyn_pawel@outlook.com>
2020-02-02 13:39:40 +0100

    change ref

--- model/restaurant.js
+++ model/restaurant.js
@@ -2,9 +2,9 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const restSchema = new Schema({
-    
-        name: {type: String, required: true, unique: true},
-        value: {type: String, required: true, unique: true},
+        itemsRes: [{type: Schema.Types.ObjectId, ref: 'RestaurantItem'}]
+        // name: {type: String, required: true, unique: true},
+        // value: {type: String, required: true, unique: true},
        
 });
 
