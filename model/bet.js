b0001fe6c349a56f2ddb9be525c82379e03c7d2c
dartho2 <wegrzyn_pawel@outlook.com>
2020-03-07 18:51:47 +0100

    a

--- model/bet.js
+++ model/bet.js
@@ -3,21 +3,7 @@ const Schema = mongoose.Schema;
 
 const betSchema = new Schema({
         date: {type: String,  required: true },
-        events: [{
-        name: {type: String, required: true},
-        type: {type: String},
-        typeYT:  {type: String},
-        typeBT:  {type: String},
-        typeVI: {type: String},
-        date: {type: String,  required: true },
-        dateControl: {type: String,  required: true },
-        league: {type: String,  required: true },
-        idEvent: {type: String,  required: true },
-        win: {type: String,  required: true },
-        vot1: {type: String,  required: false },
-        votX: {type: String,  required: false },
-        vot2: {type: String,  required: false }
-        }]
+        events: {type: Object,  required: true }
       
         
         
