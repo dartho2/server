27fad8cd31d9b6cdee4073fe08cc2f644c672608
dartho2 <wegrzyn_pawel@outlook.com>
2020-03-08 23:08:06 +0100

    bet status

--- service/betService.js
+++ service/betService.js
@@ -40,13 +40,13 @@ const betService = {
         },
     
         update: (id, betItemData) => {
-            const {date, events} = dishItemData;
+            const {date, status,events} = dishItemData;
             console.log(betItemData);
     
             return new Promise((resolve, reject) => {
                 logger.debug(`Updating content item ${id} with:\n ${JSON.stringify(betItemData, null, 2)} `);
     
-                Bet.findByIdAndUpdate(id, {date,events}, {runValidators: true})
+                Bet.findByIdAndUpdate(id, {date,status,events}, {runValidators: true})
                     .then(resolve)
                     .catch(err => reject(resolveErrorType(err)))
             });
