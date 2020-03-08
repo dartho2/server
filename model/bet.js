a8bb80a910376ce9c64b71c6dbee511fb2a78835
dartho2 <wegrzyn_pawel@outlook.com>
2020-03-09 00:33:24 +0100

    status changed

--- model/bet.js
+++ model/bet.js
@@ -3,7 +3,8 @@ const Schema = mongoose.Schema;
 
 const betSchema = new Schema({
         date: {type: String,  required: true },
-        status: {type: String, required: true },
+        status: {type: Number, required: true },
+        statusChanged: {type: Number, required: true },
         events: {type: Object,  required: true }
       
         
