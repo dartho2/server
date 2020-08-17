a1e316b9ffa1d26802c2888289a1de58d0ed7e5b
Dartho2 <entropiadarth@gmail.com>
2020-08-17 17:02:33 +0200

    bet

--- model/bet.js
+++ model/bet.js
@@ -4,6 +4,7 @@ const Schema = mongoose.Schema;
 const betSchema = new Schema({
         date: {type: String,  required: true },
         status: {type: Number, required: true },
+        events: {type: Object, required: false },
         statusChanged: {type: Number, required: true },
         rate: {type: String, required: true },
         events: {type: Object,  required: true }
