61c63345ea1bc3881f63bcfa5eab8e20e0d798fa
Dartho2 <entropiadarth@gmail.com>
2020-09-22 22:31:43 +0200

    rest
    rest

--- model/restaurant.js
+++ model/restaurant.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const restSchema = new Schema({
-        itemsres: [{type: Schema.Types.ObjectId, ref: 'RestaurantItem'}]
+        itemsres:  {type: Object, required: false}
         // name: {type: String, required: true, unique: true},
         // value: {type: String, required: true, unique: true},
        
