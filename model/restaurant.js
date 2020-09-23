60aadf3fdef00eed33954f96476deb7a334ecd61
Dartho2 <entropiadarth@gmail.com>
2020-09-23 22:38:21 +0200

    rest model
    rest model

--- model/restaurant.js
+++ model/restaurant.js
@@ -2,9 +2,9 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const restSchema = new Schema({
-        itemsres:  {type: Object, required: false}
-        // name: {type: String, required: true, unique: true},
-        // value: {type: String, required: true, unique: true},
+//         itemsres:  {type: Object, required: false}
+        name: {type: String, required: true, unique: true},
+        value: {type: String, required: true, unique: true},
        
 });
 module.exports = mongoose.model('Restaurants', restSchema);
