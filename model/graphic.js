0a0b1b8291864da190c0c3667cc90d9188f4ed76
dartho2 <wegrzyn_pawel@outlook.com>
2020-10-02 00:14:07 +0200

    patch pos v1.0.4

--- model/graphic.js
+++ model/graphic.js
@@ -4,7 +4,8 @@ const Schema = mongoose.Schema;
 const graphicSchema = new Schema({
     
         date: {type: String, required: true, unique: true},
-        items: {type: Array,  required: true }
+        items: {type: Array,  required: true },
+        restaurant: {type: Array,  required: true }
 });
 
 module.exports = mongoose.model('Graphics', graphicSchema);
